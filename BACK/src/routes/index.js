8f52f49a8d9d32e6c3d547cf04f8bb9486f5ecdc
Ricardo Hidalgo <rhidalgojimenez86@gmail.com>
2024-06-28 21:11:35 +0200

    Verificacion_de_autenticacion2

--- BACK/src/routes/index.js
+++ BACK/src/routes/index.js
@@ -2,4 +2,11 @@ import express from "express";
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('¡Bienvenido al servidor de Experiencias Diferentes!');
+  });
+
 export default router;
+
+
+  
