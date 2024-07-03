1cb4794ea1e7a146ff972739873ff49b6ca1d41a
Ana Perez <micrana23@hotmail.com>
2024-07-03 15:53:18 +0200

    validarUsuarios

---
+++ BACK/env.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASS,
+  MYSQL_DB,
+  PORT,
+  SECRET,
+  UPLOADS_DIR,
+  SMTP_HOST,
+  SMTP_PORT,
+  SMTP_USER,
+  SMTP_PASS,
+} = process.env;
+
+export {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASS,
+  MYSQL_DB,
+  PORT,
+  SECRET,
+  UPLOADS_DIR,
+  SMTP_HOST,
+  SMTP_PORT,
+  SMTP_USER,
+  SMTP_PASS,
+};
--- BACK/src/routes/index.js
+++ BACK/src/routes/index.js
@@ -1,10 +1,11 @@
+//Importamos las dependencias.
 import express from "express";
-
+//Importamos las rutas de los usuarios y de las entradas.
 import entriesRouter from "./entriesRouter.js";
 import userRouter from "./userRouter.js";
-
-const router= express.Router();
-
+//Creamos un router.
+const router = express.Router();
+//Indicamos a express dónde están las rutas de los usuarios y las entradas.
 router.use(entriesRouter);
 router.use(userRouter);
 
