0cd586e61f1248c8199cdf9104ebab4b5f066319
Ana Perez <micrana23@hotmail.com>
2024-07-04 17:25:57 +0200

    Validar_usuario

---
+++ BACK/src/models/users/insertUserModel.js
@@ -0,0 +1,64 @@
+//importamos las dependencias.
+import bcrypt from "bcrypt";
+
+// Importamos la función que devuelve una conexión con la base de datos.
+import getPool from "../../database/getPool.js";
+
+// Importamos los servicios.
+import sendMailUtil from "../../utils/sendMailUtil.js";
+
+// Función que realiza una consulta a la base de datos para crear un nuevo usuario.
+const insertUserModel = async (
+  username,
+  firstname,
+  lastname,
+  email,
+  password,
+  registrationCode
+) => {
+  const pool = await getPool();
+
+  // Buscamos en la base de datos algún usuario con ese nombre.
+  let [users] = await pool.query(`SELECT id FROM users WHERE username = ?`, [
+    username,
+  ]);
+
+  // Si existe algún usuario con ese nombre lanzamos un error.
+  if (users.length > 0) {
+    emailAlreadyRegisteredError();
+  }
+
+  // Buscamos en la base de datos algún usuario con ese email.
+  [users] = await pool.query(`SELECT id FROM users WHERE email = ?`, [email]);
+
+  // Si existe algún usuario con ese email lanzamos un error.
+  if (users.length > 0) {
+    userAlreadyRegisteredError();
+  }
+
+  // Creamos el asunto del email de verificación.
+  const emailSubject = "Activa tu usuario en Experiencias Diferentes";
+
+  // Creamos el contenido del email
+  const emailBody = `
+            ¡Bienvenid@ ${username}!
+
+            Gracias por registrarte en Experiencias Diferentes. Para activar tu cuenta, haz clic en el siguiente enlace:
+
+            <a href="http://localhost:3020/users/validate/${registrationCode}">Activar mi cuenta</a>
+        `;
+
+  // Enviamos el email de verificación al usuario.
+  await sendMailUtil(email, emailSubject, emailBody);
+
+  // Encriptamos la contraseña.
+  const hashedPass = await bcrypt.hash(password, 10);
+
+  // Insertamos el usuario.
+  await pool.query(
+    `INSERT INTO users(email, password, username, firstname, lastname, registrationCode) VALUES ( ?, ?, ?, ?, ?, ?)`,
+    [email, hashedPass, firstname, lastname, registrationCode]
+  );
+};
+
+export default insertUserModel;
