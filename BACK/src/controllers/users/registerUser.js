56bf1a1edbcdc2dbac0acfe4d98a001887b8a606
Ana Perez <micrana23@hotmail.com>
2024-07-24 17:54:32 +0200

    Modificacion_Registeruser

--- BACK/src/controllers/users/registerUser.js
+++ BACK/src/controllers/users/registerUser.js
@@ -1,8 +1,5 @@
 import randomstring from "randomstring";
 
-// Importa la conexión a la base de datos.
-import getPool from "../../database/getPool.js";
-
 // Importa el esquema de validación.
 import newUserSchema from "../../schemas/users/newUserSchema.js";
 import insertUserModel from "../../models/users/insertUserModel.js";
@@ -11,19 +8,12 @@ import validateSchemaUtil from "../../utils/validateSchemaUtil.js";
 
 // Define el controlador para registrar usuarios.
 export default async function registerUser(req, res, next) {
-  let connection;
   try {
     // Extrae el nombre de usuario, correo y contraseña del cuerpo de la solicitud.
     const { email, password, username, firstname, lastname } = req.body;
 
     // Validamos el body con Joi.
     await validateSchemaUtil(newUserSchema, req.body);
-
-    // Obtén el pool de conexiones.
-    const pool = await getPool();
-
-    // Obtén una conexión del pool.
-    connection = await pool.getConnection();
 
     // Creamos el código de registro.
     const registrationCode = randomstring.generate(30);
@@ -45,7 +35,5 @@ export default async function registerUser(req, res, next) {
     });
   } catch (err) {
     next(err);
-  } finally {
-    if (connection) connection.release();
   }
 }
