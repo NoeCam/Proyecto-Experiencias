c6479c66a30e361ccc22f64215bd329522566890
Ana Perez <micrana23@hotmail.com>
2024-08-01 17:22:50 +0200

    Hashpassword

--- BACK/src/models/users/updateUserProfileModel.js
+++ BACK/src/models/users/updateUserProfileModel.js
@@ -2,49 +2,60 @@ import getPool from "../../database/getPool.js";
 import bcrypt from "bcrypt";
 
 const updateUserProfileModel = async (userId, data) => {
+  if (!userId || !data || Object.keys(data).length === 0) {
+    throw new Error("Invalid input: No data provided for update.");
+  }
+
   const { username, firstname, lastname, email, role, password } = data;
   const pool = await getPool();
 
   const setFragments = [];
   const values = [];
 
-  if (username !== undefined) {
+  if (username) {
     setFragments.push(`username = ?`);
     values.push(username);
   }
-  if (firstname !== undefined) {
+  if (firstname) {
     setFragments.push(`firstname = ?`);
     values.push(firstname);
   }
-  if (lastname !== undefined) {
+  if (lastname) {
     setFragments.push(`lastname = ?`);
     values.push(lastname);
   }
-  if (email !== undefined) {
+  if (email) {
     setFragments.push(`email = ?`);
     values.push(email);
   }
-  if (role !== undefined) {
+  if (role) {
     setFragments.push(`role = ?`);
     values.push(role);
   }
-  if (password !== undefined) {
-    // Hash the new password
+  if (password) {
     const hashedPassword = await bcrypt.hash(password, 10);
     setFragments.push(`password = ?`);
     values.push(hashedPassword);
   }
 
+  if (setFragments.length === 0) {
+    throw new Error("No fields to update.");
+  }
+
   values.push(userId);
 
   const setClause = setFragments.join(", ");
-
   const query = `
     UPDATE users
     SET ${setClause}
     WHERE id = ?
   `;
 
+  // Log the query and values to the console for debugging
+  console.log("Generated SQL Query:", query);
+  console.log("Values:", values);
+
+  // Execute the query
   await pool.query(query, values);
 
   // Devolver los datos actualizados del usuario
